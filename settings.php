73503b60e06d1523d7bab4fd36b5cd9cc3dfecfa
PeakFiction2006 <105933774@student.swin.edu.au>
2025-05-08 13:22:03 +1000

    added php files

--- settings.php
+++ settings.php
@@ -1,6 +1,12 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pwd = "";
-$sql_db = "db_name"; // Replace with your actual DB name
+$username = "root";
+$password = "";
+$database = "cars_db"; // Replace with your actual DB name
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
