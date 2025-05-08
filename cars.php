2cc83817e0da915b0f3a3a11bd11fe53e25e1c86
PeakFiction2006 <105933774@student.swin.edu.au>
2025-05-08 13:27:25 +1000

    missing semicolon on cars.php

--- cars.php
+++ cars.php
@@ -1,26 +1,29 @@
 <?php
-    require_once"settings.php";
-    $dbconn = @mysqli_connect($host, $user, $pwd, $sql_db)
+    require_once "settings.php";
+    $dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
     if($dbconn){
-
-        query "SELECT*FROM cars";
-        if ($result){...}
-        else {...}
-        ...
-        mysqli_close($dbconn);
-        }else echo "<p>unable to connect to the db.</p>";
-
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "<tr>";
-                echo "<td>" . $row['car_id'] . "</td>";
-                echo "<td>" . $row['make'] . "</td>";
-                echo "<td>" . $row['model'] . "</td>";
-                echo "<td>" . $row['price'] . "</td>";
-                echo "<td>" . $row['yom'] . "</td>";
-                echo "</tr>";
+        $query = "SELECT * FROM cars";
+        $result = mysqli_query($dbconn, $query);
+        if ($result){
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $row['car_id'] . "</td>";
+                    echo "<td>" . $row['make'] . "</td>";
+                    echo "<td>" . $row['model'] . "</td>";
+                    echo "<td>" . $row['price'] . "</td>";
+                    echo "<td>" . $row['yom'] . "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='5'>There are no cars to display.</td></tr>";
             }
-            }else {
-            echo "<tr><td colspan='5'>There are no cars to display.</td></tr>";}
-          
+        } else {
+            // Handle query error
+            echo "<tr><td colspan='5'>Error executing query.</td></tr>";
+        }
+        mysqli_close($dbconn);
+    } else {
+        echo "<p>Unable to connect to the database.</p>";
+    }
 ?>
