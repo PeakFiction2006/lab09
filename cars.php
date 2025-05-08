23dadee5bb172f306a71eea935956e16de6eddda
PeakFiction2006 <105933774@student.swin.edu.au>
2025-05-08 13:18:04 +1000

    add warning message to cars.php

--- cars.php
+++ cars.php
@@ -9,4 +9,18 @@
         ...
         mysqli_close($dbconn);
         }else echo "<p>unable to connect to the db.</p>";
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['car_id'] . "</td>";
+                echo "<td>" . $row['make'] . "</td>";
+                echo "<td>" . $row['model'] . "</td>";
+                echo "<td>" . $row['price'] . "</td>";
+                echo "<td>" . $row['yom'] . "</td>";
+                echo "</tr>";
+            }
+            }else {
+            echo "<tr><td colspan='5'>There are no cars to display.</td></tr>";}
+          
 ?>
